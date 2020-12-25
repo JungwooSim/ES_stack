5c07ed541a3e5ca614035c57595f4a2c24ad3309
JungwooSim <cscd053@gmail.com>
2020-12-25 22:34:50 +0900

    elasticsearch mapping, preprocess edit

--- python/ES-bulk-insert-preprocess.py
+++ python/ES-bulk-insert-preprocess.py
@@ -11,13 +11,14 @@ hotel = hotel.replace(np.nan, '', regex=True) # remove set nan
 f = open("../data/ES-bulk-insert-hotel.json", 'w', encoding="utf-8")
 
 for i in hotel.index :
+    number = '"number":'+str(i+1)
     nameEdit = '"name":'+'"'+hotel.loc[i, 'name']+'"'
     areaEdit = '"area":'+'"'+hotel.loc[i, 'area']+'"'
     typeEdit = '"type":'+'"'+hotel.loc[i, 'type']+'"'
     addressEdit = '"address":'+'"'+hotel.loc[i, 'address']+'"'
     pin = hotel.loc[i, 'pin'][1:len(hotel.loc[i, 'pin'])-1]
     pin = pin.replace("'","\"");
-    f.write('{\"index\":{\"_index\":\"hotel\", \"_id\":\"'+str(i+1)+'\"}}\n')
-    f.write("{"+nameEdit+","+pin+","+areaEdit+","+typeEdit+","+addressEdit+"}\n")
+    f.write('{\"index\":{\"_index\":\"hotel\"}}\n')
+    f.write("{"+nameEdit+","+pin+","+areaEdit+","+typeEdit+","+addressEdit+","+number+"}\n")
 
 f.close()
